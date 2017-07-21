0cf90edb006a83d95435194d8348a3a8053fa96e
Parker Lusk <parkerclusk@gmail.com>
2017-07-20 19:00:36 -0600

    geolocation using tf working for fb_roof bag

--- catkin_ws/src/nasa_s2d/src/tf_frames/tf_frames.cpp
+++ catkin_ws/src/nasa_s2d/src/tf_frames/tf_frames.cpp
@@ -17,8 +17,23 @@ void TFFrames::cb_uav(const geometry_msgs::PoseStampedPtr& msg)
     tf::Transform transform;
 
     // Translate and rotate into the body (base_link) frame -- remember that this is all in ENU instead of NED
-    transform.setOrigin(tf::Vector3(msg->pose.position.x, msg->pose.position.y, msg->pose.position.z));
-    transform.setRotation(tf::Quaternion(msg->pose.orientation.x, msg->pose.orientation.y, msg->pose.orientation.z, msg->pose.orientation.w));
+    transform.setOrigin(tf::Vector3(msg->pose.position.x, msg->pose.position.y, msg->pose.position.z + 6));
+
+    tf::Quaternion quat(msg->pose.orientation.x, msg->pose.orientation.y, msg->pose.orientation.z, msg->pose.orientation.w);
+
+    if (true) {
+        // Convert the attitude quaternion to Euler angles
+        double phi, theta, psi;
+        tf::Matrix3x3(quat).getRPY(phi, theta, psi);
+
+        // Heading w.r.t ENU inertial frame (i.e., heading is zero at East)
+        psi = M_PI;
+
+        // Set the new attitude with specified heading
+        quat.setRPY(phi, theta, psi);
+    }
+    
+    transform.setRotation(quat);
     tf_br_.sendTransform(tf::StampedTransform(transform, ros::Time::now(), "fcu", "base_link")); // base_link is in ROS Body (REP 103) -- body NWU
 
 }
