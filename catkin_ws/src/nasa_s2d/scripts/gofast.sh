8f0eeb16b116b8f57236890626f473928019b511
NASA TX2 <nasatx2@nasa.com>
2018-02-26 17:35:51 -0500

    gofaster

--- catkin_ws/src/nasa_s2d/scripts/gofast.sh
+++ catkin_ws/src/nasa_s2d/scripts/gofast.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
 # Wait for 5 seconds before doing this stuff (to let mavros settle)
-sleep 5
+sleep 15
 
 # Let the world know what you are doing
 set -x
 
-rosrun mavros mavsys rate --extra1 20
-rosrun mavros mavsys rate --ext-status 10
-rosrun mavros mavsys rate --position 20
-rosrun mavros mavsys rate --raw-sensors 20
+rosrun mavros mavsys rate --all 0
+rosrun mavros mavsys rate --extra1 50
+rosrun mavros mavsys rate --ext-status 25
+rosrun mavros mavsys rate --position 50
+rosrun mavros mavsys rate --raw-sensors 50
 
 # Back to silent
 set +x
