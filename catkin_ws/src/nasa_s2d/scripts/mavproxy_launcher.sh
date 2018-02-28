2560a447a1f1ad6b2e7899642d89336fdd699c93
Parker Lusk <parkerclusk@gmail.com>
2018-02-28 08:52:44 -0700

    updated review.launch; lowered rates to fix #8

--- catkin_ws/src/nasa_s2d/scripts/mavproxy_launcher.sh
+++ catkin_ws/src/nasa_s2d/scripts/mavproxy_launcher.sh
@@ -6,4 +6,4 @@
 echo -e "\n\n\n" >> ~/mavproxy.log
 echo -e "`date`" >> ~/mavproxy.log
 
-mavproxy.py --master=/dev/ttyTHS2 --baudrate=921600 --moddebug=3 --source-system=250 --out=127.0.0.1:14550 --cmd="set streamrate 100; module load safe2ditch" >> ~/mavproxy.log
+mavproxy.py --master=/dev/ttyTHS2 --baudrate=921600 --moddebug=3 --source-system=250 --out=127.0.0.1:14550 --cmd="set streamrate 20; module load safe2ditch" >> ~/mavproxy.log
