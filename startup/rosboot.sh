47f7df0c4d93652629a6c399e2006370f84ea69d
NASA TX2 <nasatx2@nasa.com>
2018-03-01 15:52:43 -0500

    use the testname command to let the autostart know how to name the bags/logs

--- startup/rosboot.sh
+++ startup/rosboot.sh
@@ -8,26 +8,43 @@ source /home/nvidia/dev/safe2ditch/dotfiles/tx2rc
 # Reset ROS Networking -- NASA doesn't use wifi connection during flight
 ROS_IP=127.0.0.1
 
-BAGNUM_FILE=.bagnum
+BAGPATH=$HOME
 
-# Read which bag run we are on
-if [ -f "$BAGNUM_FILE" ];
+############################################
+# Attempt to get the test name for the bag #
+############################################
+
+# Start with an empty bagname
+BAGNAME=
+if [ -f ".testname" ];
 then
-	BAGNUM=`cat ${BAGNUM_FILE}`
+	BAGNAME="`cat .testname`.bag"
+
+	# If this test already exists, append the current date
+	if [ -f "$BAGPATH/$BAGNAME" ];
+	then
+		BAGNAME="`cat .testname`-`date '+%Y-%m-%d_%H:%M:%S'`.bag"
+	fi
 else
-	BAGNUM=1
+	BAGNUM_FILE=.bagnum
+
+	# Read which bag run we are on
+	if [ -f "$BAGNUM_FILE" ];
+	then
+		BAGNUM=`cat ${BAGNUM_FILE}`
+	else
+		BAGNUM=1
+	fi
+
+	# Increment the BAGNUM and save to file
+	BAGNUM=$((BAGNUM+1))
+	echo ${BAGNUM} > ${BAGNUM_FILE}
+
 fi
 
-sleep 15
+# Start the system
 rosgo &
-sleep 15
 
-# Increment the BAGNUM and save to `~/bagnum`
-BAGNUM=$((BAGNUM+1))
-echo ${BAGNUM} > ${BAGNUM_FILE}
-
-bagrecord -O "$HOME/run${BAGNUM}.bag"  # this is where you can change the bag path
-
-## Increment the BAGNUM and save to `~/bagnum`
-#BAGNUM=$((BAGNUM+1))
-#echo ${BAGNUM} > ${BAGNUM_FILE}
+# Record a bag
+sleep 5
+bagrecord -O "$BAGPATH/$BAGNAME"
