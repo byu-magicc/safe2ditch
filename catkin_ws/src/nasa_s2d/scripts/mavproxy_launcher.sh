383212cf42f9dfcb629ca84a18ff1a21a74b43ff
NASA TX2 <nasatx2@nasa.com>
2018-03-01 18:14:18 -0500

    pre-tone tone

--- catkin_ws/src/nasa_s2d/scripts/mavproxy_launcher.sh
+++ catkin_ws/src/nasa_s2d/scripts/mavproxy_launcher.sh
@@ -8,4 +8,6 @@ echo -e "`date`" >> ~/mavproxy.log
 
 # MFT90O2C16C16C16F8.A8C16C16C16F8.A4P16P8
 
+mavproxy.py --mav20 --master=/dev/ttyTHS2 --baudrate=921600 --cmd="playtune A"
+
 mavproxy.py --mav20 --master=/dev/ttyTHS2 --baudrate=921600 --moddebug=3 --source-system=250 --out=127.0.0.1:14550 --cmd="set streamrate 20; module load safe2ditch; playtune MFT90O2C16C16C16F8.A8C16C16C16F8.A4P16P8" --daemon >> ~/mavproxy.log
