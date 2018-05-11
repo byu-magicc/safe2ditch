21bc4471e09be7b356476325a9def3575169ac77
Parker Lusk <parkerclusk@gmail.com>
2018-05-10 18:56:04 -0600

    connected ros dss interface with mavros; copter moves when dss commands

--- catkin_ws/src/nasa_s2d/scripts/dss_node.py
+++ catkin_ws/src/nasa_s2d/scripts/dss_node.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 import os, sys
-import argparse
+import logging, argparse
 
 import rospy
 
@@ -10,6 +10,37 @@ from mavros_msgs.msg import RCIn, State, GlobalPositionTarget
 from mavros_msgs.srv import SetMode
 from sensor_msgs.msg import NavSatFix
 from geometry_msgs.msg import TwistStamped
+from std_msgs.msg import Float64
+
+
+class ConnectPythonLoggingToROS(logging.Handler):
+    """Connect the standard Python logging module to ROS loggin
+
+    See https://gist.github.com/nzjrs/8712011.
+    """
+
+    MAP = {
+        logging.DEBUG:    rospy.logdebug,
+        logging.INFO:     rospy.loginfo,
+        logging.WARNING:  rospy.logwarn,
+        logging.ERROR:    rospy.logerr,
+        logging.CRITICAL: rospy.logfatal
+    }
+
+    def emit(self, record):
+        try:
+            self.MAP[record.levelno]("%s: %s" % (record.name, record.msg))
+        except KeyError:
+            rospy.logerr("unknown log level %s LOG: %s: %s" % (record.levelno, record.name, record.msg))
+
+
+
+# reconnect logging calls which are children of this to the ros log system
+logging.getLogger('dss').addHandler(ConnectPythonLoggingToROS())
+# logs sent to children of dss with a level >= this will be redirected to ROS
+logging.getLogger('dss').setLevel(logging.DEBUG)
+
+
 
 class ROSInterface(dss.core.AbstractInterface):
     """ROS Interface for DSS to Autopilot Communications
@@ -22,66 +53,101 @@ class ROSInterface(dss.core.AbstractInterface):
 
         # ROS subscribers
         self.sub_global = rospy.Subscriber('mavros/global_position/global', NavSatFix, self.globalpos_cb)
+        self.sub_relalt = rospy.Subscriber('mavros/global_position/rel_alt', Float64, self.relalt_cb)
         self.sub_state = rospy.Subscriber('mavros/state', State, self.state_cb)
         self.sub_vel = rospy.Subscriber('mavros/local_position/velocity', TwistStamped, self.velocity_cb)
         self.sub_rcin = rospy.Subscriber('mavros/rc/in', RCIn, self.rcin_cb)
 
         # ROS publishers
-        # self.pub_home = rospy.Publisher('visualization/home', NavSatFix, queue_size=1, latch=True)
+        self.pub_setpos = rospy.Publisher('mavros/setpoint_raw/global', GlobalPositionTarget, queue_size=1, latch=True)
         # self.pub_mission = rospy.Publisher('visualization/mission', MarkerArray, queue_size=1, latch=True)
 
-        # Data store -- most recent data provided by ROS
-        self.data = {
-            'lat': 0,
-            'lon': 0,
-            'alt': 0,
-            'vx': 0,
-            'vy': 0,
-            'vz': 0,
-            'state': State()
-        }
+        self.reset()
 
 
     def globalpos_cb(self, msg):
         """Receive vehicle GPS information
         """
-        self.data['lat'] = msg.latitude
-        self.data['lon'] = msg.longitude
-        self.data['alt'] = msg.altitude # [m] Positive is above the WGS84 ellipsoid
+        self._data['lat'] = msg.latitude
+        self._data['lon'] = msg.longitude
+        self._data['alt'] = msg.altitude # [m] absolute, above sea level (ASL)
+
+
+    def relalt_cb(self, msg):
+        """Receive relative altitude above vehicle home message
+        """
+        self._data['rel_alt'] = msg.data
 
 
     def state_cb(self, msg):
         """Receive heartbeat/status messages
         """
-        self.data['state'] = msg
+        self.state = msg
 
 
     def velocity_cb(self, msg):
         """Receive velocity information in the body frame (FLU)
         """
-        self.data['vx'] = msg.twist.linear.x
-        self.data['vy'] = msg.twist.linear.y
-        self.data['vz'] = msg.twist.linear.z
+        self._data['vx'] = msg.twist.linear.x
+        self._data['vy'] = msg.twist.linear.y
+        self._data['vz'] = msg.twist.linear.z
 
 
     def rcin_cb(self, msg):
         """Receive RC inputs that the FCU got from the Tx
-        Expressed in raw milliseconds.
+        Expressed in raw microseconds.
         """
-        
+
         # TODO: Determine if S2D is engaged or not.
-        pass
+        if msg.channels[5] > 1500:
+            self._s2d_engaged = True
+        else:
+            self._s2d_engaged = False
+
+
+    def change_mode(self, mode):
+        rospy.wait_for_service('mavros/set_mode')
+        try:
+            set_mode = rospy.ServiceProxy('mavros/set_mode', SetMode)
+            resp = set_mode(0, mode)
+            return resp.mode_sent
+        except rospy.ServiceException as e:
+            rospy.logerr("change mode failed: %s", e)
 
     ###########################################################################
     ##                   Interface Methods to be Overridden                  ##
     ###########################################################################
 
-    def _position_lla(self):
-        return (self.data['lat'], self.data['lon'], self.data['alt'])
+    def reset(self):
+        super(ROSInterface, self).reset()
+
+        # store the latest vehicle status message from mavros
+        self.state = State()
 
 
-    def _velocity(self):
-        return (self.data['vx'], self.data['vy'], self.data['vz'])
+    def set_guidance_waypoint(self, wp):
+        
+        if self.state.mode != 'GUIDED':
+            self.change_mode('GUIDED')
+
+        msg = GlobalPositionTarget()
+
+        msg.header.stamp = rospy.Time.now()
+
+        # Use LLA with altitude being above the home position (assumes flat earth!)
+        msg.coordinate_frame = GlobalPositionTarget.FRAME_GLOBAL_REL_ALT
+        
+        # Ignore everything but LLA (4088)
+        msg.type_mask = 0b0000111111111000
+
+        # Set LLA to go to
+        msg.latitude = wp.lat
+        msg.longitude = wp.lon
+        msg.altitude = wp.alt
+
+        self.pub_setpos.publish(msg)
+
+
 
 
 
